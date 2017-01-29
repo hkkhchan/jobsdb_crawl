73a13d79adf61432c52e7478a1622cffd41ed1a3
Kenneth Chan <wkkhchan@gmail.com>
2017-01-28 22:43:39 -0500

    revise xpath and gitignore

--- jobsdb/spiders/jobs_spider.py
+++ jobsdb/spiders/jobs_spider.py
@@ -11,10 +11,10 @@ class JobsdbSpider(scrapy.Spider):
     def parse(self, response):
 			for sel in response.xpath('//div[@class="job-main"]'):
 				item = JobsItem()
-				position_list = sel.xpath('h3/a[@class="posLink"]//text()').extract()
+				position_list = sel.xpath('*/a[@class="posLink"]//text()').extract()
 				item['position'] = ''.join(position_list)
-				item['company'] = sel.xpath('div//meta[@itemprop="name"]/@content').extract()
-				item['address'] = sel.xpath('div//span[@itemprop="address"]/text()').extract()
-				item['link'] = sel.xpath('h3/a[@class="posLink"]/@href').extract()
+				item['company'] = sel.xpath('*//meta[@itemprop="name"]/@content').extract()
+				item['address'] = sel.xpath('*//span[@itemprop="address"]/text()').extract()
+				item['link'] = sel.xpath('*/a[@class="posLink"]/@href').extract()
 				yield item
 
