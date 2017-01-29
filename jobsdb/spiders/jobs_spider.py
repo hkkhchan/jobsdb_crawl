9344e8c1cd508f75544b00b687bf98c86ae575b8
Kenneth Chan <wkkhchan@gmail.com>
2017-01-29 03:56:37 -0500

    recursive capture

--- jobsdb/spiders/jobs_spider.py
+++ jobsdb/spiders/jobs_spider.py
@@ -5,7 +5,7 @@ class JobsdbSpider(scrapy.Spider):
     name = "jobsdb"
     allowed_domains = ["hk.jobsdb.com"]
     start_urls = [
-        "http://hk.jobsdb.com/HK/EN/Search/FindJobs?KeyOpt=COMPLEX&JSRV=1&RLRSF=1&JobCat=1&SearchFields=Positions,Companies&Key=programmer"
+		"http://hk.jobsdb.com/HK/en/Search/FindJobs?AD=3&Blind=1&Host=J&JSRV=1&Key=programmer&KeyOpt=COMPLEX&SearchFields=Positions%2cCompanies&page=0"
     ]
 
     def parse(self, response):
@@ -17,4 +17,9 @@ class JobsdbSpider(scrapy.Spider):
 				item['address'] = sel.xpath('*//span[@itemprop="address"]/text()').extract()
 				item['link'] = sel.xpath('*/a[@class="posLink"]/@href').extract()
 				yield item
+			
+			next_page = response.xpath('//a[@class="pagebox pagebox-next"]/@href')
+			
+			if next_page:
+				yield scrapy.Request(next_page[0].extract(), self.parse)
 
